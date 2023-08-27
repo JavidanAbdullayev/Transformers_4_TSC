92ac648e6f7139b67d74dcc67ce9f531ef0fa975
JavidanAbdullayev <cavidan5889@gmail.com>
2023-08-27 15:23:20 +0200

    PyTorch version of TF scripts (sunday, auot 27)

--- PyTorch/Models/Attention.py
+++ PyTorch/Models/Attention.py
@@ -7,48 +7,70 @@ class Attention(nn.Module):
     def __init__(self, emb_size, num_heads, dropout):
         super().__init__()
         self.num_heads = num_heads
-        self.scale = emb_size ** -0.5   
+        self.scale = emb_size ** -0.5
+        # self.to_qkv = nn.Linear(inp, inner_dim * 3, bias=False)
+        print('emb_size: ', emb_size)
+        self.key = nn.Linear(1, emb_size, bias=True)
+        self.value = nn.Linear(1, emb_size, bias=True)
+        self.query = nn.Linear(1, emb_size, bias=True)
 
-        self.key = nn.Linear(emb_size, emb_size, bias=False)
-        self.value = nn.Linear(emb_size, emb_size, bias=False)
-        self.query = nn.Linear(emb_size, emb_size, bias=False)
-
-        self.dropout = nn.Dropout(dropout)
-        self.to_out = nn.LayerNorm(emb_size)
-
+        self.to_out = nn.Linear(emb_size, 1)
+        # self.to_out = nn.Dropout(dropout)
+        # self.to_out = nn.LayerNorm(emb_size)
     def forward(self, x):
-        print('\n\n Attention start')
+
         batch_size, seq_len, _ = x.shape
-        print('x shape in attention: ', x.shape)
+        # print('\n\n\n')
+        print('Input shape of attention, x shape: ', x.shape)
+        # print('ATTENTION MODULE')
+        # print('batch_size: ', batch_size)
+        # print('seq_len: ', seq_len)
+        # print('_: ', _)
+        # print("self.key(x).reshape(batch_size, seq_len, self.num_heads, -1) shape: ",  self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).shape)
+        # print("self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1) shape: ",  self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1).shape)
+       
+        # print('self.value(x) shape: ', self.value(x).shape)
+        # print('self.value(x).reshape(batch_size, seq_len, self.num_heads, -1) shape: ', self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).shape)
+        # print('self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2) shape: ', self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2).shape)
+        # print('\n\n\n')
+        
+        
         k = self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
-        print('Output shape of self.key(x):  ', self.key(x).shape)
-        print('Output shape of self.key(x).reshape(batch_size, seq_len, self.num_heads, -1):  ', self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).shape)
-        print('Output shape of self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1):  ', self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1).shape)
-
         v = self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
-        print('\nOutput shape of self.value(x):  ', self.value(x).shape)
-        print('Output shape of self.value(x).reshape(batch_size, seq_len, self.num_heads, -1):  ', self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).shape)
-        print('Output shape of self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2):  ', self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2).shape)
-
-
-        # print('Output shape of value:  ', self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2).shape)
         q = self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
-        print('\nOutput shape of self.query(x):  ', self.query(x).shape)
-        print('Output shape of self.query(x).reshape(batch_size, seq_len, self.num_heads, -1):  ', self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).shape)
-        print('Output shape of self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2):  ', self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2).shape)
+        # k,v,q shape = (batch_size, num_heads, seq_len, d_head)
 
         attn = torch.matmul(q, k) * self.scale
-        print('\ntorch.matmul(q, k) shape: ', torch.matmul(q, k).shape)
-        attn = nn.functional.softmax(attn,dim=-1)
-        print('attn shape after softmax: ', attn.shape)
+        print('Attention shape: ', attn.shape)
+        # print('attn shape: ', attn.shape)
+        # attn shape (seq_len, seq_len)
+        attn = nn.functional.softmax(attn, dim=-1)  
+
+        # import matplotlib.pyplot as plt
+        # plt.plot(x[0, :, 0].detach().cpu().numpy())
+        # plt.show()
 
         out = torch.matmul(attn, v)
         print('out shape: ', out.shape)
+        # out.shape = (batch_size, num_heads, seq_len, d_head)
         out = out.transpose(1, 2)
-        print('out after transpose: ', out.shape)
+        print('out shape  11: ', out.shape)
+        # out.shape == (batch_size, seq_len, num_heads, d_head)
         out = out.reshape(batch_size, seq_len, -1)
-        print('out after reshape: ', out.shape)
-        out = self.to_out(out)
-        print('\n\n Attention end shape: ', out.shape)
+        print('out shape  22: ', out.shape)
 
+        # out.shape == (batch_size, seq_len, d_model)
+        out = self.to_out(out)
+        print('Last output shape: ', out.shape)
         return out
+
+class CustomeAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads):
+        super().__init__()
+        self.multihead_attention = nn.MultiheadAttention(embed_dim, num_heads)
+
+    def forward(self, x):
+        x = x.permute(1, 0, 2)
+        output, _ = self.multihead_attention(x, x, x)
+        output = output.permute(1, 0, 2)
+        return output
--- PyTorch/Models/model.py
+++ PyTorch/Models/model.py
@@ -1,7 +1,7 @@
 import numpy as np
 from torch import nn
 
-from Models.Attention import Attention
+from Models.Attention import Attention, CustomeAttention
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -25,15 +25,15 @@ class Transformer(nn.Module):
     def __init__(self, config, num_classes):
         super().__init__()
         # Parameters Initialization -----------------------------------------------
-        print('\n\n\n')
+        # print('\n\n\n')
         channel_size, seq_len = config['Data_shape'][1], config['Data_shape'][2]
-        print('channel_size:  ', channel_size, '  seq_len:  ', seq_len)
+        # print('channel_size:  ', channel_size, '  seq_len:  ', seq_len)
         emb_size = config['emb_size']
-        print('emb_size: ', 16)
+        # print('emb_size: ', 16)
         num_heads = config['num_heads']
-        print('num_heads: ', num_heads)
+        # print('num_heads: ', num_heads)
         dim_ff = config['dim_ff']
-        print('dim_ffL: ', dim_ff)
+        # print('dim_ffL: ', dim_ff)
         self.Fix_pos_encode = config['Fix_pos_encode']
         self.Rel_pos_encode = config['Rel_pos_encode']
         # Embedding Layer -----------------------------------------------------------
@@ -47,55 +47,91 @@ class Transformer(nn.Module):
         elif config['Fix_pos_encode'] == 'Learn':
             pass
 
-        self.LayerNorm1 = nn.LayerNorm(emb_size, eps=1e-5)
-        self.LayerNorm2 = nn.LayerNorm(emb_size, eps=1e-5)
+        self.LayerNorm1 = nn.LayerNorm(1, eps=1e-5)
+        self.LayerNorm2 = nn.LayerNorm(1, eps=1e-5)
 
         self.attention_layer = Attention(emb_size, num_heads, config['dropout'])
+        self.custom_attention_layer = CustomeAttention(emb_size, num_heads)
         
-        self.FeedForward = nn.Sequential(
-            nn.Linear(emb_size, dim_ff),
-            nn.ReLU(),
-            nn.Dropout(config['dropout']),
-            nn.Linear(dim_ff, emb_size),
-            nn.Dropout(config['dropout']),
-        )
+        self.feed_norm = nn.LayerNorm(1, eps=1e-5)
+        self.feed_conv1d_1 = nn.Conv1d(in_channels=1, out_channels=dim_ff, kernel_size=1,  stride=1, padding='same')
+        self.feed_drop = nn.Dropout(0)
+        self.feed_conv1d_2 = nn.Conv1d(in_channels=dim_ff, out_channels=1, kernel_size=1,  stride=1, padding='same')
+
+        # self.FeedForward = nn.Sequential(
+        #     nn.LayerNorm(1, eps=1e-5),
+        #     nn.Conv1d(in_channels=251, out_channels=dim_ff, kernel_size=1,  stride=1, padding='same'),
+        #     nn.Dropout(0),
+        #     nn.Conv1d(in_channels=dim_ff, out_channels=1, kernel_size=1,  stride=1, padding='same'),
+        # )
 
         self.gap = nn.AdaptiveAvgPool1d(1)
         self.flatten = nn.Flatten()
-        self.out = nn.Linear(emb_size, num_classes)
-        print('\n\n\n')
-
+        self.dense1 = nn.Linear(251, 128,)
+        self.relu = nn.ReLU()   
+        self.dropout = nn.Dropout(0)
+        self.out = nn.Linear(128, num_classes)
+        # print('\n\n\n')
 
     def forward(self, x):
-        print('Inputn shape: ', x.shape)
-        x_src = self.embed_layer(x.permute(0, 2, 1))
-        print('New shape of input: ', x.permute(0, 2, 1).shape)
-        print('Shape of x_src after embedding layer: ', x_src.shape)
+        # print('Inputn shape: ', x.shape)
+        # x_src = self.embed_layer(x.permute(0, 2, 1))
+        # print('Input shape before embedding: ', x.shape)
+        # x = self.embed_layer(x.permute(0, 2, 1))
+
+        x = x.permute(0, 2, 1)
+        print('Encode shape: ', x.shape)
+        x = self.LayerNorm1(x)
+        # print('Input shape 111: ', x.shape)
+        # print('Original x shape: ', x.shape)
+        # print('x_src shape: ', x_src.shape)
+        # print('New shape of input: ', x.permute(0, 2, 1).shape)
+        # print('Shape of x_src after embedding layer: ', x_src.shape)
         # if self.Fix_pos_encode != None:
         #     x_src = self.Fix_pos_encode(x_src)
-
-        att = x_src + self.attention_layer(x_src)  # residual conn btw input and out of attn
+        # x = self.LayerNorm1(x)
+        print('Input shape of Attention PyTorch: ', x.shape)
         
-        print('Output shape of attention layer: ', att.shape)
-        att = self.LayerNorm1(att)  # first layer norm in enc block
-        print('Out shape after first Normalization layer: ', att.shape)
+        att = x + self.attention_layer(x)  # residual conn btw input and out of attn
+        
+        print('Output of MHA: PyTorch: ', att.shape)
 
+        att = self.dropout(att)
+        # print('Output shape of attention layer: ', att.shape)
+        # print('Output shape of attention layer: ', att.shape)
+        # att = self.LayerNorm1(att)  # first layer norm in enc block
+        # print('Out shape after first Normalization layer: ', att.shape)
 
-        out = att + self.FeedForward(att)  # res conn btw out of attn and ffn
-        print('Out shap of self.FeedForward(att): ', self.FeedForward(att).shape)
-        out = self.LayerNorm2(out)  # second layer norm in enc block
+        print('res shape before norm: ', att.shape)
+        feed_norm = self.feed_norm(att)
+        print('feed_norm shape: ', feed_norm.shape)
+        print('permuted feed_norm shape: ', feed_norm.permute(0, 2, 1).shape)
 
-        print('Output shape of encoder block: ', out.shape)
-        out = out.permute(0, 2, 1)
-        print('Output shape of encoder block after permutation: ', out.shape)
+        feed_conv1d_1 = self.feed_conv1d_1(feed_norm.permute(0, 2, 1))
+        print('feed_conv1d_1 shape: ', feed_conv1d_1.shape)
+
+        feed_drop = self.feed_drop (feed_conv1d_1)
+        feed_conv1d_2 = self.feed_conv1d_2(feed_drop)
+        print('feed_conv1d_2 shape: ', feed_conv1d_2.shape)
+        print('attention shape: ', att.shape)
+
+        out = att + feed_conv1d_2.permute(0, 2, 1)  # res conn btw out of attn and ffn
+        # print('Out shap of self.FeedForward(att): ', out.shape)
+        # out = self.LayerNorm2(out)  # second layer norm in enc block
+
+        # print('Output shape of encoder block: ', out.shape)
+        # out = out.permute(0, 2, 1)
+        # print('Output shape of encoder block after permutation: ', out.shape)
         
         out = self.gap(out)
-        print('Output of gap layer: ', out.shape)
+        # print('Output of gap layer: ', out.shape)
         out = self.flatten(out)
-        print('Outut of flatten layer: ', out.shape)
+        # print('Outut of flatten layer: ', out.shape)
+        out = self.relu(self.dense1(out))
+        out = self.dropout(out)
         out = self.out(out)
-        print('Final output shape: ', out.shape)
-        print('\n\n\n')
+        # print('Final output shape: ', out.shape)
+        # print('\n\n\n')
         return out
 
 
